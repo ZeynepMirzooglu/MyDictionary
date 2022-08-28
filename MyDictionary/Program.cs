9c918072f1de31c548921007292697284c569f22
Zeynep Mirzooğlu <zeynepmirzooglu@gmail.com>
2022-08-29 00:51:41 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneCodes = new Dictionary<int, string>()
+            {
+                {312,"Ankara"},
+                {482,"Mardin"},
+                {322,"Adana"},
+                {412,"Diyarbakır"},
+                {424,"Elazığ"},
+                {442,"Erzurum"},
+                {326,"Hatay"},
+            };
+            phoneCodes.Add(212,"İstanbul(Avr)");
+            foreach (var phoneCity in phoneCodes)
+            {
+                Console.WriteLine(phoneCity);
+            }
+        }
+        
+    }
+}
+
